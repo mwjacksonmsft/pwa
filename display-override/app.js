7123b9f5b03a07e517da408b9f73e8ad67bd7f69
Mike Jackson <mjackson@microsoft.com>
2020-07-10 15:34:53 -0700

    fixup display-override

--- display-override/app.js
+++ display-override/app.js
@@ -1,7 +1,7 @@
 
 const registerServiceWorker = async () => {
   try {
-      await navigator.serviceWorker.register('/pwa/display-fullscreen/sw.js', { scope: '/pwa/display-fullscreen/'});
+      await navigator.serviceWorker.register('/pwa/display-override/sw.js', { scope: '/pwa/display-override/'});
       console.log('Service worker registered');
   } catch (e) {
       console.log(`Registration failed: ${e}`);
