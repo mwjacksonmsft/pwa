2339364a9fcba15e8d81f84a4272f6c0beb47d22
Mike Jackson <mjackson@microsoft.com>
2020-07-14 23:45:30 -0700

    update sw

--- display-standalone/sw.js
+++ display-standalone/sw.js
@@ -4,5 +4,21 @@ this.addEventListener('install', async (event) => {
 });
 
 this.addEventListener('fetch', (event) => {
-  event.respondWith(fetch(event.request));
+  if (event.request.method != 'GET') return;
+
+  event.respondWith(async function() {
+    // Try to get the response from a cache.
+    const cache = await caches.open('dynamic-v1');
+    const cachedResponse = await cache.match(event.request);
+
+    if (cachedResponse) {
+      // If we found a match in the cache, return it, but also
+      // update the entry in the cache in the background.
+      event.waitUntil(cache.add(event.request));
+      return cachedResponse;
+    }
+
+    // If we didn't find a match in the cache, use the network.
+    return fetch(event.request);
+  }());
 });
