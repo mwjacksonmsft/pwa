a84bff28f18ca6ea12854b743670e88abf956a3b
Mike Jackson <mjackson@microsoft.com>
2021-02-10 11:04:31 -0800

    add geoloc

--- get-user-media/app.js
+++ get-user-media/app.js
@@ -1,10 +1,10 @@
 
 const registerServiceWorker = async () => {
   try {
-      await navigator.serviceWorker.register('/pwa/get-user-media/sw.js', { scope: '/pwa/get-user-media/'});
-      console.log('Service worker registered');
+    await navigator.serviceWorker.register('/pwa/get-user-media/sw.js', { scope: '/pwa/get-user-media/' });
+    console.log('Service worker registered');
   } catch (e) {
-      console.log(`Registration failed: ${e}`);
+    console.log(`Registration failed: ${e}`);
   }
 }
 
@@ -28,7 +28,17 @@ function startVideoCapture() {
     .catch(function (err) { console.log(err.name + ": " + err.message); }); // always check for errors at the end.
 }
 
+function getlocation() {
+  navigator.geolocation.getCurrentPosition(showLoc);
+}
+function showLoc(pos) {
+  document.getElementById("location").innerHTML = "Latitude: " + pos.coords.latitude +
+    "<br>Longitude: " + pos.coords.longitude;
+}
+
+
 window.addEventListener('DOMContentLoaded', (event) => {
   document.querySelector('#showVideo').addEventListener('click', e => startVideoCapture());
+  getlocation();
 });
 
