b17efc3225646d285d24daa220f23b7414d02066
Mike Jackson <mjackson@microsoft.com>
2021-09-11 15:01:21 -0700

    fixup dir

--- display-standalone-no-icons/app.js
+++ display-standalone-no-icons/app.js
@@ -1,7 +1,7 @@
 
 const registerServiceWorker = async () => {
   try {
-      await navigator.serviceWorker.register('/pwa/display-standard-no-icons/sw.js', { scope: '/pwa/display-standard-no-icons/'});
+      await navigator.serviceWorker.register('/pwa/display-standalone-no-icons/sw.js', { scope: '/pwa/display-standalone-no-icons/'});
       console.log('Service worker registered');
   } catch (e) {
       console.log(`Registration failed: ${e}`);
